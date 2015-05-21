3941fedc4342bfbe74afae7b79b4cc8826e0acae
Manfred Diaz <manfred.diaz@jobsity.com>
2015-05-21 16:50:36 -0500

    3.11 Showing CreatedOn Date

--- gemstore/index.js
+++ gemstore/index.js
@@ -25,7 +25,8 @@
   });
   app.controller('ReviewController', function() {
     this.review = {};
-    this.addReview = function(product) {
+    this.addReview = function(product){
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
