44b41f827416c35a39f537aaac64fe69846dcc92
Manfred Diaz <manfred.diaz@jobsity.com>
2015-05-22 09:54:11 -0500

    4.3 Creating an Element Directive

--- gemstore/index.js
+++ gemstore/index.js
@@ -31,6 +31,12 @@
       this.review = {};
     };
   });
+  app.directive('productDescription', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
   var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
